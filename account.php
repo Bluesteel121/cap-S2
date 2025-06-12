d4ac0c510a4d5cff81e4a7f4bc090df6b5b9c1da
raynesmark721 <raynesmark721@gmail.com>
2025-06-12 12:48:55 +0000

    XD

--- account.php
+++ account.php
@@ -10,6 +10,7 @@
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
     <div class="bg-white p-6 rounded-lg shadow-md">
         <h1 class="text-2xl font-bold text-center text-blue-600">This is a simple Tailwind page</h1>
+    
     </div>
 </body>
 </html>
