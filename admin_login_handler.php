16f6cb128af2225661ae494b20e8595105f80b5f
raynesmark721 <raynesmark721@gmail.com>
2025-06-19 06:00:40 +0000

    xD

--- admin_login_handler.php
+++ admin_login_handler.php
@@ -18,7 +18,7 @@ function logAdminLoginAttempt($message) {
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING); // Note: Passwords shouldn't be sanitized in this way if hashing
+    $password = filter_input(INPUT_POST, 'password'); // Removed FILTER_SANITIZE_STRING
 
     // Basic validation
     if (empty($username) || empty($password)) {
